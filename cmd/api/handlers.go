7c310053e762980e49b87c0400a084e317afba2a
Kostaaa1 <kostaarsic123@gmail.com>
2025-01-21 23:57:50 +0100

    .

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,129 +1,129 @@
 package main
 
-// func (a *app) Index(w http.ResponseWriter, r *http.Request) {
-// 	html := `
-// 	<!DOCTYPE html>
-// 	<html lang="en">
-// 	<head>
-// 		<meta charset="UTF-8">
-// 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-// 		<title>Hello World</title>
-// 	</head>
-// 	<body>
-// 		<h1>Hello, World!</h1>
-// 	</body>
-// 	</html>
-// 	`
-// 	w.Header().Set("Content-Type", "text/html")
-// 	w.WriteHeader(http.StatusOK)
-// 	w.Write([]byte(html))
-// }
+import (
+	"context"
+	"errors"
+	"net/http"
+	"net/url"
 
-// func (a *app) GetAll(w http.ResponseWriter, r *http.Request) {
-// 	clientID, err := createClientID(r)
-// 	if err != nil {
-// 		a.serverErrorResponse(w, r, err)
-// 		return
-// 	}
+	"github.com/gorilla/mux"
+)
 
-// 	links, err := a.storage.GetAll(ctx, domain.QueryParams{ClientID: clientID})
-// 	if err != nil {
-// 		a.serverErrorResponse(w, r, err)
-// 		return
-// 	}
+func (a *app) GetAll(w http.ResponseWriter, r *http.Request) {
+	clientID, err := createClientID(r)
+	if err != nil {
+		a.serverErrorResponse(w, r, err)
+		return
+	}
 
-// 	if err := a.writeJSON(w, http.StatusOK, links, nil); err != nil {
-// 		a.serverErrorResponse(w, r, err)
-// 	}
-// }
+	ctx := context.Background()
 
-// func (a *app) validateCreateInput(clientID, URL string, alias *string) error {
-// 	_, err := url.Parse(URL)
-// 	if err != nil {
-// 		return errors.New("format of the provided URL is not valid")
-// 	}
-// 	if err := a.storage.ValidateOriginalURL(ctx, clientID, URL); err != nil {
-// 		return err
-// 	}
+	links, err := a.storage.List(ctx, QueryParams{ClientID: clientID})
+	if err != nil {
+		a.serverErrorResponse(w, r, err)
+		return
+	}
 
-// 	if *alias == "" {
-// 		*alias = createHashAlias(clientID, URL, 8)
-// 	} else {
-// 		if err := a.storage.ValidateAlias(ctx, *alias); err != nil {
-// 			return err
-// 		}
-// 	}
+	if err := a.writeJSON(w, http.StatusOK, links, nil); err != nil {
+		a.serverErrorResponse(w, r, err)
+	}
+}
 
-// 	return nil
-// }
+func (a *app) validateCreateInput(ctx context.Context, clientID, URL string, alias *string) error {
+	_, err := url.Parse(URL)
+	if err != nil {
+		return errors.New("format of the provided URL is not valid")
+	}
 
-// func (a *app) Create(w http.ResponseWriter, r *http.Request) {
-// 	var input struct {
-// 		URL   string `json:"url"`
-// 		Alias string `json:"alias"`
-// 	}
-// 	if err := a.readJSON(r, &input); err != nil {
-// 		a.serverErrorResponse(w, r, err)
-// 		return
-// 	}
+	if err := a.storage.CheckOriginalURL(ctx, clientID, URL); err != nil {
+		return err
+	}
 
-// 	clientID, err := createClientID(r)
-// 	if err != nil {
-// 		a.errorResponse(w, r, http.StatusBadRequest, err.Error())
-// 		return
-// 	}
+	if *alias == "" {
+		*alias = createHashAlias(clientID, URL, 8)
+	} else {
+		if err := a.storage.CheckAlias(ctx, *alias); err != nil {
+			return err
+		}
+	}
 
-// 	if err := a.validateCreateInput(clientID, input.URL, &input.Alias); err != nil {
-// 		a.errorResponse(w, r, http.StatusBadRequest, err.Error())
-// 		return
-// 	}
+	return nil
+}
 
-// 	tl, err := tinylink.New("http://localhost:3000", input.URL, input.Alias)
-// 	if err != nil {
-// 		a.serverErrorResponse(w, r, err)
-// 		return
-// 	}
+func (a *app) Create(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		URL   string `json:"url"`
+		Alias string `json:"alias"`
+	}
 
-// 	qp := domain.QueryParams{ClientID: clientID, Alias: input.Alias}
-// 	if err := a.storage.Create(ctx, tl, qp); err != nil {
-// 		a.errorResponse(w, r, http.StatusBadRequest, err.Error())
-// 		return
-// 	}
+	if err := a.readJSON(r, &input); err != nil {
+		a.serverErrorResponse(w, r, err)
+		return
+	}
 
-// 	if err := a.writeJSON(w, http.StatusOK, tl, nil); err != nil {
-// 		a.serverErrorResponse(w, r, err)
-// 	}
-// }
+	clientID, err := createClientID(r)
+	if err != nil {
+		a.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
 
-// func (a *app) DeleteTinylink(w http.ResponseWriter, r *http.Request) {
-// 	clientID, _ := createClientID(r)
-// 	tinylink := mux.Vars(r)["alias"]
+	ctx := context.Background()
 
-// 	if err := a.storage.Delete(ctx, domain.QueryParams{ClientID: clientID, Alias: tinylink}); err != nil {
-// 		a.errorResponse(w, r, http.StatusBadRequest, "failed to delete tinylink")
-// 		return
-// 	}
+	if err := a.validateCreateInput(ctx, clientID, input.URL, &input.Alias); err != nil {
+		a.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
 
-// 	if err := a.writeJSON(w, http.StatusOK, envelope{"msg": "tinylink succesfully deleted"}, nil); err != nil {
-// 		a.serverErrorResponse(w, r, err)
-// 	}
-// }
+	tl, err := NewTinylink("http://localhost:3000", input.URL, input.Alias)
+	if err != nil {
+		a.serverErrorResponse(w, r, err)
+		return
+	}
 
-// func (a *app) Redirect(w http.ResponseWriter, r *http.Request) {
-// 	clientID, err := createClientID(r)
-// 	if err != nil {
-// 		a.errorResponse(w, r, http.StatusBadRequest, err.Error())
-// 		return
-// 	}
+	qp := QueryParams{ClientID: clientID, Alias: input.Alias}
 
-// 	tinylink := mux.Vars(r)["alias"]
+	if err := a.storage.Save(ctx, tl, qp); err != nil {
+		a.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
 
-// 	tl, err := a.storage.Get(ctx, domain.QueryParams{ClientID: clientID, Alias: tinylink})
-// 	if err != nil {
-// 		a.errorResponse(w, r, http.StatusInternalServerError, "no data under this hash")
-// 		return
-// 	}
+	if err := a.writeJSON(w, http.StatusOK, tl, nil); err != nil {
+		a.serverErrorResponse(w, r, err)
+	}
+}
 
-// 	w.Header().Set("Location", tl.OriginalURL)
-// 	w.WriteHeader(http.StatusFound)
-// }
+func (a *app) DeleteTinylink(w http.ResponseWriter, r *http.Request) {
+	clientID, _ := createClientID(r)
+	tinylink := mux.Vars(r)["alias"]
+
+	ctx := context.Background()
+
+	if err := a.storage.Delete(ctx, QueryParams{ClientID: clientID, Alias: tinylink}); err != nil {
+		a.errorResponse(w, r, http.StatusBadRequest, "failed to delete tinylink")
+		return
+	}
+
+	if err := a.writeJSON(w, http.StatusOK, envelope{"msg": "tinylink succesfully deleted"}, nil); err != nil {
+		a.serverErrorResponse(w, r, err)
+	}
+}
+
+func (a *app) Redirect(w http.ResponseWriter, r *http.Request) {
+	clientID, err := createClientID(r)
+	if err != nil {
+		a.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	tinylink := mux.Vars(r)["alias"]
+	ctx := context.Background()
+
+	tl, err := a.storage.Get(ctx, QueryParams{ClientID: clientID, Alias: tinylink})
+	if err != nil {
+		a.errorResponse(w, r, http.StatusInternalServerError, "no data under this hash")
+		return
+	}
+
+	w.Header().Set("Location", tl.OriginalURL)
+	w.WriteHeader(http.StatusFound)
+}
--- internal/tinylink/infrastructure/db/redis/connection.go
+++
@@ -1,8 +0,0 @@
-package redis
-
-import "github.com/redis/go-redis/v9"
-
-func NewConnection(addr string, opts *redis.Options) *redis.Client {
-	opts.Addr = addr
-	return redis.NewClient(opts)
-}
